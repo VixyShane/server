d83cb7f9084e8213c6d10f61d99ead5feeb4788a
Konstantyn <k.zakharchenko@wittix.com>
2025-08-06 12:57:49 +0300

    7

--- src/main/kotlin/com/faigenbloom/chicks/Application.kt
+++ src/main/kotlin/com/faigenbloom/chicks/Application.kt
@@ -6,16 +6,25 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
-import kotlinx.serialization.json.Json
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.util.reflect.*
-import java.io.File
+import kotlinx.serialization.json.Json
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    install(CORS) {
+        allowHost("localhost:4200", schemes = listOf("http"))
+        allowHost("vixyshane.com", schemes = listOf("https"))
+        allowHeader(HttpHeaders.ContentType)
+        allowHeader(HttpHeaders.Authorization)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+    }
     install(StatusPages) {
         exception<Throwable> { call, cause ->
             println("❌ Exception occurred: ${cause.message}")
@@ -28,18 +37,6 @@ fun Application.module() {
 
         }
     }
-/*
-    install(CORS) {
-        anyHost()
-        allowHeader(HttpHeaders.ContentType)
-        allowHeader(HttpHeaders.Authorization)
-        allowMethod(HttpMethod.Get)
-        allowMethod(HttpMethod.Post)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
-    }
-*/
-
     configureRouting()
     install(ContentNegotiation) { json(Json { ignoreUnknownKeys = true }) }
     PayPalCredentials.PAYPAL_CLIENT_ID = environment.config.property("paypal.clientId").getString()
